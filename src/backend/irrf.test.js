982615f75fb7e31f13b819f9af4e5e3fe2b8e9cf
Bruno Leão <99328889+brunomleao@users.noreply.github.com>
2023-09-29 15:49:40 -0300

    prova resolvida

--- src/backend/irrf.test.js
+++ src/backend/irrf.test.js
@@ -1,22 +1,24 @@
-const irrf = require('./irrf')
+const irrf = require('./irrf');
 
 describe('[Obter faixas IRRF]', () => {
-    test('deve retornar 05 elementos', () => {
-        const faixas = irrf.obterFaixas();
-        expect(faixas.length).toEqual(5);
-    });
+  test('deve retornar 05 elementos', () => {
+    const faixas = irrf.obterFaixas();
+    expect(faixas.length).toEqual(5);
+  });
 });
 
 describe('[Encontrar aliquota IRRF]', () => {
-    test('deve retornar zero', () => {
-        const faixas = irrf.obterFaixas();
-        const result = irrf.faixaContemplaValor(100, faixas[0]);
-        expect(result).toEqual(true);
-    });
+  test('deve retornar zero', () => {
+    const faixas = irrf.obterFaixas();
+    const result = irrf.faixaContemplaValor(100, faixas[0]);
+    expect(result).toEqual(true);
+  });
 });
 
-
 describe('[Calcular desconto IRRF]', () => {
-   
-    
+  test('deve retornar 45.80', () => {
+    const salarioBase = 2722.61;
+    const result = irrf.calculardesconto(salarioBase);
+    expect(result).toEqual(45.8);
+  });
 });
